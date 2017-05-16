167fd09ffd160b82ecbe438d56115ced6602e45f
ajinkya <ajinkya.badve@xpressbees.com>
2017-05-16 11:32:06 +0530

    master activity added in master branch for test

---
+++ GitHubTest/app/src/main/java/badve/ajnkya/githubtest/MasterBranchActivity.java
@@ -0,0 +1,13 @@
+package badve.ajnkya.githubtest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MasterBranchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_master_branch);
+    }
+}
